e49d43101b0856b6ddabf2d8a17fac83c45ad9db
MartijnEisses <33952780+MartijnEisses@users.noreply.github.com>
2021-06-03 19:00:28 +0200

    started working on integrating meta_data but something went wrong with the last part. see description
    https://auth0.com/docs/quickstart/spa/angular/02-calling-an-api#provide-the-http-interceptor

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { concatMap, tap, pluck } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-account',
@@ -7,10 +9,23 @@ import { AuthService } from '@auth0/auth0-angular';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
+  metadata = {};
 
-  constructor(public auth: AuthService) { }
+  // Inject both AuthService and HttpClient
+  constructor(public auth: AuthService, private http: HttpClient) {}
 
   ngOnInit(): void {
-  }
-
+    // this.auth.user$
+    // .pipe(
+    //   concatMap((user) =>
+    //     // Use HttpClient to make the call
+    //     this.http.get(
+    //       encodeURI(`https://dev--4x0kppm.eu.auth0.com/api/v2/users/${user.sub}`)
+    //     )
+    //   ),
+    //   pluck('user_metadata'),
+    //   tap((meta) => (this.metadata = meta))
+    // )
+    // .subscribe();
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,32 +8,61 @@ import { TimelineComponent } from './timeline/timeline.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AccountComponent } from './account/account.component';
 
+// Angular
 import { AuthModule } from '@auth0/auth0-angular';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthHttpInterceptor } from '@auth0/auth0-angular';
 
 const routes: Routes = [
-  {path: 'timeline', component: TimelineComponent},
-  {path: 'account', component: AccountComponent},
-  {path: 'profile', component: ProfileComponent},
-  {path: '', redirectTo: '/app', pathMatch: 'full'}
-]
+  { path: 'timeline', component: TimelineComponent },
+  { path: 'account', component: AccountComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: '', redirectTo: '/app', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     TimelineComponent,
     ProfileComponent,
-    AccountComponent
+    AccountComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AuthModule.forRoot({
+      // The domain and clientId
       domain: 'dev--4x0kppm.eu.auth0.com',
-      clientId: 'O4eRDhDCAGEK7WGvYkw0L2l2iKr84fc1'
+      clientId: 'O4eRDhDCAGEK7WGvYkw0L2l2iKr84fc1',
+      // Request this audience at user authentication time
+      audience: 'https://dev--4x0kppm.eu.auth0.com/api/v2/',
+
+      // Request this scope at user authentication time
+      scope: 'read:current_user',
+
+      // Specify configuration for the interceptor
+      httpInterceptor: {
+        allowedList: [
+          {
+            // Match any request that starts 'https://dev--4x0kppm.eu.auth0.com/api/v2/' (note the asterisk)
+            uri: 'https://dev--4x0kppm.eu.auth0.com/api/v2/*',
+            tokenOptions: {
+              // The attached token should target this audience
+              audience: 'https://dev--4x0kppm.eu.auth0.com/api/v2/',
+
+              // The attached token should have these scopes
+              scope: 'read:current_user',
+            },
+          },
+        ],
+      },
     }),
     RouterModule.forRoot(routes),
     ReactiveFormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

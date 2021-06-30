db9837963a7e00bb4b9c6d54f7a4b8b3686cd7f3
MartijnEisses <33952780+MartijnEisses@users.noreply.github.com>
2021-06-30 09:44:26 +0200

    Register and settingspage are the same. also did some styling to the forms

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { AuthService } from '@auth0/auth0-angular';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-settings',
@@ -9,14 +11,39 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class SettingsComponent implements OnInit {
 
-  settingsForm = new FormGroup({
-    firstname: new FormControl(''),
-    lastname: new FormControl(''),
-    birthday: new FormControl(''),
-    bio: new FormControl(''),
-  });
-  constructor() { }
-
+  form: FormGroup;
+  constructor(private auth: AuthService, private api: ApiService, private router: Router, private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: [''],
+      first_name: [''],
+      last_name: [''],
+      bio: [''],
+      gender: [''],
+      image: File
+    })
+   }
   ngOnInit(): void {
   }
+uploadFile(event: Event){
+  const file = (event.target as HTMLInputElement).files![0];
+  this.form.patchValue({
+    image: file
+  });
+  this.form.get('image')!.updateValueAndValidity()
+}
+
+
+submitForm(){
+  var formData: any = new FormData();
+  formData.append("email", this.form.get('email')!.value);
+  formData.append("first_name", this.form.get('first_name')!.value);
+  formData.append("last_name", this.form.get('last_name')!.value);
+  formData.append("user_bio", this.form.get('bio')!.value)
+  formData.append("gender", this.form.get('gender')!.value)
+  formData.append("image", this.form.get('image')!.value);
+  this.api.createUser(formData).subscribe(
+    (response) => console.log(response),
+    (error) => console.log(error)
+  )
+}
 }
